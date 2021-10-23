0cd4a37d7e7769a1115acb0ecea965adf23750b5
Mark Standen <mark@markstanden.dev>
2021-10-23 06:33:15 +0100

    submitting via js eventlistener

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,20 @@
-import { Page } from './Page';
 import './style.css';
-import header from './html/header.ts.html';
-import cableInfoForm from './html/cable-info-form.ts.html';
-import cableInfoResult from './html/cable-info-result.ts.html';
-import footer from './html/footer.ts.html';
 
-const pageContents = new Page(cableInfoForm).render();
+const form = document.querySelector<HTMLFormElement>(
+   '#cable-info-form',
+)!;
 
-const app = document.querySelector<HTMLDivElement>('#app')!;
+form.addEventListener('submit', e => onFormSubmit(e), {
+   once: true,
+});
 
-app.innerHTML = `
-${header}
-<main id="main-content">
-   ${pageContents}
-</main>
-${footer}
-`;
-
-function onFormSubmit() {
-   const result = new Page(cableInfoResult).render();
+function onFormSubmit(event: Event): void {
+   event.preventDefault();
    const mainContent =
       document.querySelector<HTMLDivElement>(
          '#main-content',
       )!;
    mainContent.innerHTML = `
-      ${result}
+      <div style="font-size:5rem">Just stick a 10mm² in.</div>
    `;
 }
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,1 @@
 /// <reference types="vite/client" />
-
-declare module '*.ts.html' {
-   const value: string;
-   export default value;
-}
