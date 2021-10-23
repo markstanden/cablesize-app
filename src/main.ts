d7738c483932bc9d9faddda4bb9b2fb0f586b6b9
Mark Standen <mark@markstanden.dev>
2021-10-23 09:37:07 +0100

    test func

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,29 @@
-import './style.css';
+import "./style.css";
 
 const form = document.querySelector<HTMLFormElement>(
-   '#cable-info-form',
+   "#cable-info-form",
 )!;
 
-form.addEventListener('submit', e => onFormSubmit(e), {
-   once: true,
+form.addEventListener("submit", e => onFormSubmit(e), {
+   once: false,
 });
 
-function onFormSubmit(event: Event): void {
+async function onFormSubmit(event: Event): Promise<void> {
    event.preventDefault();
    const mainContent =
       document.querySelector<HTMLDivElement>(
-         '#main-content',
+         "#main-content",
       )!;
+
+   const response = await fetch(
+      "./netlify/functions/hello",
+   ).then(async res => {
+      console.log(res.body);
+
+      console.log(await res.json());
+   });
+
    mainContent.innerHTML = `
-      <div style="font-size:5rem">Just stick a 10mm² in.</div>
+      <div style="font-size:5rem">${response}</div>
    `;
 }
